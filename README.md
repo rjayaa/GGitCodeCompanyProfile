9024185040245abddca28cc07633a2a99aed9b94
rjayaa <rjaya.lelana03@gmail.com>
2023-09-21 00:03:16 +0700

    tests

--- README.md
+++
@@ -1,1 +0,0 @@
-# ggitcodeCompanyProfile
---
+++ company-profile/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.css";
+
+// import { motion } from "framer-motion"
+
+const App = () => (
+    <div className="flex justify-center text-[#c6a]">
+      <h1>gGITcode</h1>
+    </div>
+);
+
+
+export default App
